7d5e08c7889a1a32e0f358f28fdfa0ca30234d2a
22-daw-ericapastor <117342144+22-daw-ericapastor@users.noreply.github.com>
2022-11-25 20:53:20 +0100

    header add security

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -37,7 +37,7 @@
                     <a class="nav-link py-3 px-0 px-lg-3 rounded" href="#contact">Contacto</a>
                 </li>
                 <?php if (isset($_SESSION['__user'])): ?>
-                    <?php if ($_SESSION['__user']['permissions'] === 1): ?>
+                    <?php if (isset($_SESSION['__user']['permissions']) && $_SESSION['__user']['permissions'] === 1): ?>
                         <li class="nav-item mx-0 mx-lg-1">
                             <a class="nav-link py-3 px-0 px-lg-3 rounded" href="?">Gestionar recetas</a>
                         </li>
